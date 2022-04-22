5f7cdd7f7794895052295b3fa557ed6354f1c52d
Santiago Martinez <100589811+Equimenta@users.noreply.github.com>
2022-04-22 08:59:30 -0500

    Finalized project with better of 5

--- config.js
+++ config.js
@@ -16,29 +16,46 @@ function computerPlay(){
     }
 
 }
-computer = computerPlay()
-let player = prompt('Escoge una boludoooooo') 
-player = player.toLocaleLowerCase() 
 
-while (player != 'rock' && player != 'paper' && player != 'scissors'){
-    player = prompt('Pero escoge entre piedra papel o tijera ome imbecil')
+
+let countplayer = 0
+let countcomputer = 0
+
+while (countplayer<5 && countcomputer<5){ //Contador para jugar hasta que alguno gane 5 veces 
+    computer = computerPlay()
+    let player = prompt('Escoge una boludoooooo') 
+    player = player.toLocaleLowerCase() 
+
+    while (player != 'rock' && player != 'paper' && player != 'scissors'){ //Validar si el jugador hizo una eleccion valoida
+        player = prompt('Pero escoge entre piedra papel o tijera ome imbecil')
+        console.log(player)
+    }
     console.log(player)
+    console.log(computer)
+    if (player==computer){ //Definir quien gana segun las elecciones previas
+        alert('Drawwwww maricaaaaaa')
+    }
+    else if(player=='rock' && computer=='scissors'){
+        alert('Ganasteeee, roca le gana a tijera')
+        countplayer++
+    }
+    else if(player=='paper' && computer=='rock'){
+        alert('Ganasteeee, papel le gana a roca')
+        countplayer++
+    }
+    else if (player=='scissors' && computer=='paper'){
+        alert('Ganasteeee, tijera le gana a papel')
+        countplayer++
+    }
+    else{
+        alert('Nooo marica perdiste, sos una mierda')
+        countcomputer++
+    }
+    alert('Player: '+countplayer+' vs computer: '+countcomputer)
 }
-console.log(player)
-console.log(computer)
-
-if (player==computer){
-    alert('Drawwwww maricaaaaaa')
-}
-else if(player=='rock' && computer=='scissors'){
-    alert('Ganasteeee, roca le gana a tijera')
-}
-else if(player=='paper' && computer=='rock'){
-    alert('Ganasteeee, papel le gana a roca')
-}
-else if (player=='scissors' && computer=='paper'){
-    alert('Ganasteeee, tijera le gana a papel')
+if (countplayer==5){
+    alert('GANASTEEEEE HIJUEPUTAA')
 }
 else{
-    alert('Nooo marica perdiste, sos una mierda')
+    alert('PERDISTEEEE HIJUEPUTAA')
 }
